faaac3e0a64cd555f9273bab8268f915f4224ca0
Christopher Simão Souza <chrissimao68@gmail.com>
2026-06-05 17:30:52 -0300

    migra banco para postgresql

--- prisma/migrations/20260605202940_init_postgres/migration.sql
+++ prisma/migrations/20260605202940_init_postgres/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "PontoTuristico" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "titulo" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "descricao" TEXT NOT NULL,
@@ -9,18 +9,24 @@ CREATE TABLE "PontoTuristico" (
     "endereco" TEXT,
     "categoria" TEXT NOT NULL,
     "publicado" BOOLEAN NOT NULL DEFAULT true,
-    "criadoEm" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "atualizadoEm" DATETIME NOT NULL
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizadoEm" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PontoTuristico_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "FotoPonto" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "url" TEXT NOT NULL,
     "legenda" TEXT,
     "pontoTuristicoId" INTEGER NOT NULL,
-    CONSTRAINT "FotoPonto_pontoTuristicoId_fkey" FOREIGN KEY ("pontoTuristicoId") REFERENCES "PontoTuristico" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "FotoPonto_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "PontoTuristico_slug_key" ON "PontoTuristico"("slug");
+
+-- AddForeignKey
+ALTER TABLE "FotoPonto" ADD CONSTRAINT "FotoPonto_pontoTuristicoId_fkey" FOREIGN KEY ("pontoTuristicoId") REFERENCES "PontoTuristico"("id") ON DELETE CASCADE ON UPDATE CASCADE;
